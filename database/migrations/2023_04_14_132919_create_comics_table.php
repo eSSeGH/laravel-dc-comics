137aa6a738858e75da4512d16aa5fccc3d16002f
eSSeGH <ste.virgadaula@gmail.com>
2023-04-14 16:04:35 +0200

    SETUP migration+ model+seeder+controller+views add

---
+++ database/migrations/2023_04_14_132919_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 5, 2)->unsigned();
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,48 @@
+<header class="main-header">
+
+    <div class="container flex-center">
+
+        <div class="logo">
+            <img src="{{ Vite::asset('../../images/dc-logo.png') }}" alt="dc logo horizontal">
+        </div>
+
+        <nav class="navbar">
+
+            <ul style='display: contents'>
+                <li class="nav-link">
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">COMICS</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">MOVIES</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">TV</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">GAMES</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">VIDEOS</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">FANS</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">NEWS</a>
+                </li>
+                <li class="nav-link">
+                    <a href="">SHOP</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </div>
+
+</header>
