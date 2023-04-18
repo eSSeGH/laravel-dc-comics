7e06e47bf4eca2c59742d99e30a0724b5e3e06a4
eSSeGH <ste.virgadaula@gmail.com>
2023-04-18 14:45:55 +0200

    FEAT + STYLE marketing & subscrip. sections added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,42 @@
 <footer>
+    {{-- MARKETING SECTION --}}
+    <section class="marketing">
+
+        <div class="container">
+
+            <div class="marketing-cards d-flex align-items-center">
+
+                <ul style="display: contents;">
+                    <li class="card d-flex align-items-center">
+                        <img src="{{ asset('dc-img/buy-comics-digital-comics.png') }}" alt="">
+                        <span>DIGITAL COMICS</span>
+                    </li>
+                    <li class="card d-flex align-items-center">
+                        <img src="{{ asset('dc-img/buy-comics-merchandise.png') }}" alt="">
+                        <span>DC MERCHANDISE</span>
+                    </li>
+                    <li class="card d-flex align-items-center">
+                        <img src="{{ asset('dc-img/buy-comics-subscriptions.png') }}" alt="">
+                        <span>SUBSCRIPTION</span>
+                    </li>
+                    <li class="card d-flex align-items-center">
+                        <img src="{{ asset('dc-img/buy-comics-shop-locator.png') }}" alt="">
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+                    <li class="card d-flex align-items-center">
+                        <img src="{{ asset('dc-img/buy-dc-power-visa.svg') }}" alt="">
+                        <span>DC POWER VISA</span>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    {{-- FOOTER LINKS SECTION --}}
     <section class="link-footer">
 
         <div class="container">
@@ -64,6 +102,54 @@
         </div>
 
         <img class="footer-logo-bg" src="{{ asset('dc-img/dc-logo-bg.png') }}" alt="">
+
+        {{-- SUBSCRIPTION SECTION --}}
+        <section class="subscription-bar">
+
+            <div class="container d-flex align-items-center">
+    
+                <button class="sign-up-btn">
+                    SIGN-UP NOW!
+                </button>
+    
+                <div class="follow-us d-flex align-items-center">
+                    <span>
+                        FOLLOW US
+                    </span>
+    
+                    <ul class="d-flex align-items-center">
+                        <li>
+                            <a href="">
+                                <img src={{ asset('dc-img/footer-facebook.png') }} alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={{ asset('dc-img/footer-twitter.png') }} alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={{ asset('dc-img/footer-youtube.png') }} alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={{ asset('dc-img/footer-pinterest.png') }} alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={{ asset('dc-img/footer-periscope.png') }} alt="">
+                            </a>
+                        </li>
+                    </ul>
+    
+                </div>
+    
+            </div>
+    
+        </section>
 
     </section>
 </footer>
